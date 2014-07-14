8b0832e611c021dab960fd5aad9b89173aa39ea2
lvlong <lvlong@chukong-inc.com>
2014-07-14 10:46:32 +0800

    rename

---
+++ cocos/3d/CCAABB3D.h
@@ -0,0 +1,118 @@
+/****************************************************************************
+ Copyright (c) 2014 Chukong Technologies Inc.
+ 
+ http://www.cocos2d-x.org
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+ ****************************************************************************/
+
+#ifndef __CC_AABB_H__
+#define __CC_AABB_H__
+
+#include "2d/CCNode.h"
+
+NS_CC_BEGIN
+
+/**
+ * Defines a class describe 3-dimensional axis-aligned bounding box.
+ */
+class AABB : public Node
+{
+public:
+    /**
+     * create AABB box
+	 */
+    static AABB* create();
+    static AABB* create(const Vec3& min, const Vec3& max);
+    
+    /**
+     * Gets the center point of the bounding box.
+     */
+    Vec3 getCenter();
+
+    /**
+     * Gets the corners of the bounding box in the specified array.
+     */
+    void getCorners(Vec3 (* dst)[8]) const;
+
+    /**
+     * Tests whether this bounding box intersects the specified bounding object.
+     */
+    bool intersects(const AABB& box) const;
+
+    /**
+     * check whether the point is in.
+     */
+    bool containPoint(const Vec3& point) const;
+
+    /**
+     * Sets this bounding box to the smallest bounding box
+     * that contains both this bounding object and the specified bounding box.
+     */
+    void merge(const AABB& box);
+
+    /**
+     * Sets this bounding box to the specified values.
+     */
+    void set(const Vec3& min, const Vec3& max);
+    
+    /**
+     * Gets min and max value.
+     */
+    void get(Vec3& min, Vec3& max);
+    
+    /**
+     * reset min and max value.
+     */
+    void reset();
+
+    /**
+     * Transforms the bounding box by the given transformation matrix.
+     */
+    void transform(const C3DMatrix& matrix);
+    
+ CC_CONSTRUCTOR_ACCESS:
+    /**
+     * Constructor.
+     */
+    AABB();
+    
+    /**
+     * Constructor.
+     */
+    AABB(const Vec3& min, const Vec3& max);
+    
+    /**
+     * Constructor.
+     */
+    AABB(const AABB& box);
+    
+    /**
+     * Destructor.
+     */
+    ~AABB();
+    
+protected:
+    Vec3 _min;
+    Vec3 _max;
+};
+
+NS_CC_END
+
+#endif
