41ce5564f6b88b209511fd31d87d467d742f5dae
songmiao <songmiao@chukong-inc.com>
2014-07-30 10:53:11 +0800

    modify align

--- tests/cpp-tests/Classes/Sprite3DTest/Sprite3DTest.h
+++ tests/cpp-tests/Classes/Sprite3DTest/Sprite3DTest.h
@@ -254,6 +254,7 @@ public:
     void addNewSpriteWithCoords(Vec2 p);
     void onTouchesEnded(const std::vector<Touch*>& touches, Event* event);
 };
+
 class Sprite3DWithCollisonTest : public Sprite3DTestDemo
 {
 public:
@@ -261,24 +262,24 @@ public:
     Sprite3DWithCollisonTest();
     virtual std::string title() const override;
     virtual std::string subtitle() const override;
-	virtual void update(float dt) override;
-	void addNewSpriteWithCoords(Vec2 p);
-	void onTouchesEnded(const std::vector<Touch*>& touches, Event* event);
+    virtual void update(float dt) override;
+    void addNewSpriteWithCoords(Vec2 p);
+    void onTouchesEnded(const std::vector<Touch*>& touches, Event* event);
 protected:
-	void reachEndCallBack1();
-	void reachEndCallBack2();
+    void reachEndCallBack1();
+    void reachEndCallBack2();
     void goCallBack1();
-	void goCallBack2();
-    cocos2d::Sprite3D* _sprite1;
-	cocos2d::Sprite3D* _sprite2;
-	MoveTo* _moveActionGo1;
-	MoveTo* _moveActionGo2;
-    MoveTo* _moveActionBack1;
-	MoveTo* _moveActionBack2;
-	DrawNode3D* _drawAABB1;
-	DrawNode3D* _drawAABB2;
-    OBB* _obb1;
-	OBB* _obb2;
-	bool _hasCollider;
+    void goCallBack2();
+    cocos2d::Sprite3D*          _sprite1;
+    cocos2d::Sprite3D*          _sprite2;
+    MoveTo*                     _moveActionGo1;
+    MoveTo*                     _moveActionGo2;
+    MoveTo*                     _moveActionBack1;
+    MoveTo*                     _moveActionBack2;
+    DrawNode3D*                 _drawAABB1;
+    DrawNode3D*                 _drawAABB2;
+    OBB*                        _obb1;
+    OBB*                        _obb2;
+    bool                        _hasCollider;
 };
 #endif
