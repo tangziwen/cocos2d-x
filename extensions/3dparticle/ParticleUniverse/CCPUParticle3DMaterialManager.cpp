3b24a13d0eed50de46eeb1c094be492088f201a6
yangxiao <yangxiao@cocos2d-x.org>
2014-12-31 14:08:01 +0800

    mac & ios work

--- extensions/3dparticle/ParticleUniverse/CCPUParticle3DMaterialManager.cpp
+++ extensions/3dparticle/ParticleUniverse/CCPUParticle3DMaterialManager.cpp
@@ -155,11 +155,7 @@ bool PUParticle3DMaterialCache::loadMaterialsFromSearchPaths( const std::string 
 //          }
 //      }
 #elif (CC_TARGET_PLATFORM == CC_PLATFORM_IOS || CC_TARGET_PLATFORM == CC_PLATFORM_MAC)
-    for (auto iter : FileUtils::getInstance()->getSearchPaths())
-    {
-        std::string fullPath = iter;
-        ftw(fullPath.c_str(), iterPath, 500);
-    }
+    ftw(fileFolder.c_str(), iterPath, 500);
 #endif
 
     return state;
